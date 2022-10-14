e831ff2e0f270730c98579bc873af5a35119fe88
xDev <papesock1@gmail.com>
2022-10-14 22:46:48 +0200

    soap services

---
+++ ClientWS/src/fr/lip6/org/calculatrice/AddResponse.java
@@ -0,0 +1,54 @@
+
+package fr.lip6.org.calculatrice;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for addResponse complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="addResponse">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="Resultat" type="{http://www.w3.org/2001/XMLSchema}double"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "addResponse", propOrder = {
+    "resultat"
+})
+public class AddResponse {
+
+    @XmlElement(name = "Resultat")
+    protected double resultat;
+
+    /**
+     * Gets the value of the resultat property.
+     * 
+     */
+    public double getResultat() {
+        return resultat;
+    }
+
+    /**
+     * Sets the value of the resultat property.
+     * 
+     */
+    public void setResultat(double value) {
+        this.resultat = value;
+    }
+
+}
---
+++ ClientWS/src/fr/lip6/org/calculatrice/Multiplier.java
@@ -0,0 +1,74 @@
+
+package fr.lip6.org.calculatrice;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for multiplier complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="multiplier">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="Operande1" type="{http://www.w3.org/2001/XMLSchema}double"/>
+ *         &lt;element name="Operande2" type="{http://www.w3.org/2001/XMLSchema}double"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "multiplier", propOrder = {
+    "operande1",
+    "operande2"
+})
+public class Multiplier {
+
+    @XmlElement(name = "Operande1")
+    protected double operande1;
+    @XmlElement(name = "Operande2")
+    protected double operande2;
+
+    /**
+     * Gets the value of the operande1 property.
+     * 
+     */
+    public double getOperande1() {
+        return operande1;
+    }
+
+    /**
+     * Sets the value of the operande1 property.
+     * 
+     */
+    public void setOperande1(double value) {
+        this.operande1 = value;
+    }
+
+    /**
+     * Gets the value of the operande2 property.
+     * 
+     */
+    public double getOperande2() {
+        return operande2;
+    }
+
+    /**
+     * Sets the value of the operande2 property.
+     * 
+     */
+    public void setOperande2(double value) {
+        this.operande2 = value;
+    }
+
+}
